f92ae1721c78add7d63dc6b8a259b90718ce8224
Dylan Aumond <dylanaumond@gmail.com>
2022-03-21 16:49:54 +0100

    ajout de routes

--- controllers/Projects.js
+++ controllers/Projects.js
@@ -16,9 +16,11 @@ export const createProject = async (req, res) => {
   const { libelle, state, description, customer } = req.body;
   const imgs = [];
   const images = req.files;
-  images.forEach((img) => {
-    imgs.push(img.filename);
-  });
+  if (images) {
+    images.forEach((img) => {
+      imgs.push(img.filename);
+    });
+  }
 
   try {
     const project = await Projects.create({
@@ -64,8 +66,43 @@ export const getProjects = async (req, res) => {
 export const getProjectByLibelle = async (req, res) => {
   const { libelle } = req.params;
   try {
-    const project = await Projects.findOne({ libelle: libelle });
+    const project = await Projects.findOne({ libelle: libelle }).populate(
+      "technologies"
+    );
     if (project != null) return res.status(200).json(project);
     res.status(404).json({ message: "this project isn't on our website" });
   } catch (error) {}
+};
+
+export const addTechnoToProject = async (req, res) => {
+  const { projectId, technologyId } = req.body;
+  try {
+    const project = await Projects.findById(projectId);
+    await project.update({
+      $addToSet: { technologies: technologyId },
+      new: true,
+      upsert: true,
+    });
+    project.save();
+    res.status(201).json({ message: "techno has been haded to this project" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "Something went wrong" });
+  }
+};
+
+export const removeTechnoFromProject = async (req, res) => {
+  const { projectId, technologyId } = req.body;
+  try {
+    const project = await Projects.findById(projectId);
+    await project.update({
+      $pull: { technologies: technologyId },
+    });
+    res
+      .status(201)
+      .json({ message: "techno has been remove from this project" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: "Something went wrong" });
+  }
 };
--- routes/Projects.js
+++ routes/Projects.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
+  addTechnoToProject,
   createProject,
   deleteProject,
   getProjectByLibelle,
   getProjects,
+  removeTechnoFromProject,
 } from "../controllers/Projects.js";
 import uploadImage from "../middleware/uploadImage.js";
 
@@ -12,6 +14,8 @@ const router = express.Router();
 router.post("/", uploadImage.array("imgs"), createProject);
 router.get("/", getProjects);
 router.get("/:libelle", getProjectByLibelle);
+router.patch("/technology/add", addTechnoToProject);
+router.patch("/technology/remove", removeTechnoFromProject);
 router.delete("/:id", deleteProject);
 
 export default router;
